c23a70571e0c5b2fbaa260ff23f5552824d149d5
Diegopj22 <dpj2202@gmail.com>
2022-10-14 12:28:13 +0200

    comprobación str

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,7 +1,17 @@
+from tkinter import Y
+
+
 class Punto:
     def __init__(self,x=0,y=0):
         self.x=x
         self.y=y
+    def __str__(self) :
+        
+        return(self.x,self.y)
+        
+punto=(8,9)
+print(str(punto))
 
+    
         
     
