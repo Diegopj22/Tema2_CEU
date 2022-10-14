7ab9f988dc6d0fa1891e893b55fe4d581fb99b8b
Diegopj22 <dpj2202@gmail.com>
2022-10-14 12:46:48 +0200

    método cuadrante

--- ejercicio1.py
+++ ejercicio1.py
@@ -8,9 +8,22 @@ class Punto:
     def __str__(self) :
         
         return(self.x,self.y)
+    
+    def cuadrante(self):
+        if self.x>0 and self.y>0:
+            print("El punto pertenece al primer cuadrante")
+        elif self.x < 0 and self.y > 0:
+            print("El punto pertenece al segundo cuadrante")
+        elif self.x < 0 and self.y < 0:
+            print("El punto pertenence al tercer cuadrante")
+        else:
+            print("El punto pertenece al cuarto cuadrante")
+
+            
+
+
         
-punto=(8,9)
-print(str(punto))
+
 
     
         
