1c04b35ec4288261c48a807731233653b33879aa
Diegopj22 <dpj2202@gmail.com>
2022-10-15 18:53:55 +0200

    convertimos la primera letra en mayúscula

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,10 @@
 texto="un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
-texto=texto.split("#")
-for i in texto:
-    print(i)
+texto2=texto.split("#")
+for i in texto2:
+    
+    if i == 0:
+        texto2[i]=texto2[i]+"..."  
+
+    print(i.capitalize())
 
 
