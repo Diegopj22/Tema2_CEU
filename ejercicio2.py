48ff107002fceb01bcab6de692e03b796e19a06b
Diegopj22 <dpj2202@gmail.com>
2022-10-15 19:21:16 +0200

    contador y frase

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,10 +1,15 @@
 texto="un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"
 texto2=texto.split("#")
-for i in texto2:
-    
-    if i == 0:
-        texto2[i]=texto2[i]+"..."  
+for contador,frase in enumerate(texto2):
+    texto2[contador]=frase.capitalize()
+    if contador == 0:
+        texto2[contador]=texto2[contador]+"..."
+    else:
+        texto2[contador]="-"+texto2[contador]+"."
 
-    print(i.capitalize())
+ 
+    
+        
+    
 
 
