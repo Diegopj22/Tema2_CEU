4832d7d6def7b3e36d6273282db95ddf68d84239
Diegopj22 <dpj2202@gmail.com>
2022-10-16 10:55:46 +0200

    eliminamos los números impares

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,8 +3,17 @@ lista =[2,4,8,-4,22,3,2,5,8]
 def modificar(lista):
     resultado=[]
     for i in lista:
-        if i not in resultado:
+        if i not in resultado and i % 2 == 0:
             resultado.append(i)
     resultado.sort(reverse=True)       
+    
+    
+    
+        
+
+    
     return (resultado)
+
 print(modificar([2,4,8,-4,22,3,2,5,8]))
+
+            
