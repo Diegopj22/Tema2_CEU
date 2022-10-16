db2993afb5b5106d5ba129d65167d9a8ef86219a
Diegopj22 <dpj2202@gmail.com>
2022-10-16 10:24:32 +0200

    ordenar lista de mayor a menor

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,6 +5,6 @@ def modificar(lista):
     for i in lista:
         if i not in resultado:
             resultado.append(i)
-            
+    resultado.sort(reverse=True)       
     return (resultado)
 print(modificar([2,4,8,-4,22,3,2,5,8]))
